d5064282777b75bf618ebaaf557b14493c903713
Ciel <63270879+ephemeralpage@users.noreply.github.com>
2023-03-01 22:45:52 +0800

    Add files via upload

--- leetcode.cpp
+++ leetcode.cpp
@@ -118,7 +118,7 @@ struct DSU {
 		int ARes = findParent(A);
 		int BRes = findParent(B);
 //		if (ARes == BRes) {
-//			return false;//已经是一个集合了
+//			return false;	//已经是一个集合了
 //		}
 		if (eachSize[ARes] < eachSize[BRes]) {
 			std::swap(ARes, BRes);
@@ -126,5 +126,177 @@ struct DSU {
 		parents[BRes] = ARes;
 		eachSize[ARes] += eachSize[BRes];
 		return true;
+	}
+};
+
+
+// 区间加减Lazy线段树
+
+class SegmentTree_LazyRangeAdd {
+private:
+	std::vector<long long> tree, lazy;
+	std::vector<int>* arr;
+	int n, root, n4, end;
+
+	void _maintain(int left, int right, int index) {
+		int mid = left + (right - left) / 2;
+		if (left != right && lazy[index]) {
+			lazy[index * 2] += lazy[index];
+			lazy[index * 2 + 1] += lazy[index];
+			tree[index * 2] += lazy[index] * (mid - left + 1);
+			tree[index * 2 + 1] += lazy[index] * (right - mid);
+			lazy[index] = 0;
+		}
+	}
+
+	long long _range_sum(int L, int R, int left, int right, int index) {
+		if (L <= left && right <= R) {
+			return tree[index];
+		}
+		int mid = left + (right - left) / 2;
+		long long sum = 0;
+		_maintain(left, right, index);
+		if (L <= mid) {
+			sum += _range_sum(L, R, left, mid, index * 2);
+		}
+		if (R > mid) {
+			sum += _range_sum(L, R, mid + 1, right, index * 2 + 1);
+		}
+		return sum;
+	}
+
+	void _range_add(int L, int R, int val, int left, int right, int index) {
+		if (L <= left && right <= R) {
+			lazy[index] += val;
+			tree[index] += (right - left + 1) * val;
+			return;
+		}
+		int mid = left + (right - left) / 2;
+		_maintain(left, right, index);
+		if (L <= mid) {
+			_range_add(L, R, val, left, mid, index * 2);
+		}
+		if (R > mid) {
+			_range_add(L, R, val, mid + 1, right, index * 2 + 1);
+		}
+		tree[index] = tree[index * 2] + tree[index * 2 + 1];
+	}
+
+	void _build(int left, int right, int index) {
+		if (left == right) {
+			tree[index] = (*arr)[left];
+			return;
+		}
+		int mid = left + (right - left) / 2;
+		_build(left, mid, index * 2);
+		_build(mid + 1, right, index * 2 + 1);
+		tree[index] = tree[index * 2] + tree[index * 2 + 1];
+	}
+
+public:
+	explicit SegmentTree_LazyRangeAdd(std::vector<int>& v) {
+		n = v.size();
+		n4 = n * 4;
+		tree = std::vector<long long>(n4, 0);
+		lazy = std::vector<long long>(n4, 0);
+		arr = &v;
+		end = n - 1;
+		root = 1;
+		_build(0, end, 1);
+		arr = nullptr;
+	}
+
+	long long range_sum(int left, int right) {
+		return _range_sum(left, right, 0, end, root);
+	}
+
+	void range_add(int left, int right, int val) {
+		_range_add(left, right, val, 0, end, root);
+	}
+};
+
+
+// 区间修改Lazy线段树
+
+class SegmentTree_LazyRangeSet {
+private:
+	std::vector<long long> tree, lazy;
+	std::vector<int>* arr;
+	int n, root, n4, end;
+
+	void _maintain(int left, int right, int index) {
+		int mid = left + (right - left) / 2;
+		if (left != right && lazy[index]) {
+			lazy[index * 2] = lazy[index];
+			lazy[index * 2 + 1] = lazy[index];
+			tree[index * 2] = lazy[index] * (mid - left + 1);
+			tree[index * 2 + 1] = lazy[index] * (right - mid);
+			lazy[index] = 0;
+		}
+	}
+
+	long long _range_sum(int L, int R, int left, int right, int index) {
+		if (L <= left && right <= R) {
+			return tree[index];
+		}
+		int mid = left + (right - left) / 2;
+		long long sum = 0;
+		_maintain(left, right, index);
+		if (L <= mid) {
+			sum += _range_sum(L, R, left, mid, index * 2);
+		}
+		if (R > mid) {
+			sum += _range_sum(L, R, mid + 1, right, index * 2 + 1);
+		}
+		return sum;
+	}
+
+	void _range_set(int L, int R, int val, int left, int right, int index) {
+		if (L <= left && right <= R) {
+			lazy[index] = val;
+			tree[index] = (right - left + 1) * val;
+			return;
+		}
+		int mid = left + (right - left) / 2;
+		_maintain(left, right, index);
+		if (L <= mid) {
+			_range_set(L, R, val, left, mid, index * 2);
+		}
+		if (R > mid) {
+			_range_set(L, R, val, mid + 1, right, index * 2 + 1);
+		}
+		tree[index] = tree[index * 2] + tree[index * 2 + 1];
+	}
+
+	void _build(int left, int right, int index) {
+		if (left == right) {
+			tree[index] = (*arr)[left];
+			return;
+		}
+		int mid = left + (right - left) / 2;
+		_build(left, mid, index * 2);
+		_build(mid + 1, right, index * 2 + 1);
+		tree[index] = tree[index * 2] + tree[index * 2 + 1];
+	}
+
+public:
+	explicit SegmentTree_LazyRangeSet(std::vector<int>& v) {
+		n = v.size();
+		n4 = n * 4;
+		tree = std::vector<long long>(n4, 0);
+		lazy = std::vector<long long>(n4, 0);
+		arr = &v;
+		end = n - 1;
+		root = 1;
+		_build(0, end, 1);
+		arr = nullptr;
+	}
+
+	long long range_sum(int left, int right) {
+		return _range_sum(left, right, 0, end, root);
+	}
+
+	void range_set(int left, int right, int val) {
+		_range_set(left, right, val, 0, end, root);
 	}
 };
